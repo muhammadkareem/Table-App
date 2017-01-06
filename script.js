7155a383613b4f32df22fe452c2f1a95ef1f3d4c
Muhammad kareem <abdulkareem0315@gmail.com>
2017-01-06 23:38:08 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+
+
+function btn1()
+	{
+		
+var inpt = document.getElementById("inptv").value;
+var ol1 = document.getElementById("ol1");
+var li1 = document.createElement("li");
+var tx;
+
+for( var i = 1 ; i <=10 ; i ++ )
+	{
+       
+	 tx = document.createTextNode(inpt + "*" + i + "=" + inpt*i + "<br>");
+      li1.appendChild(tx);
+	  
+		}
+		
+   	  ol1.appendChild(li1);
+	}
+	
+	
+	
+		
+
